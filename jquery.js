0b0aef9361f333f29660079791c96952e4a322a4
yoelalf <yoelalfandi@gmail.com>
2023-07-03 22:32:48 +0700

    pre installation done

--- jquery.js
+++ jquery.js
@@ -1,4 +1,5 @@
 let link = 'https://sdk-os-static.mihoyo.com/hk4e_global/mdk/launcher/api/resource?launcher_id=10&key=gcStgarh'
+// let link = 'offline.json'
 let object_parent;
 $(document).ready(function() {
     $.getJSON(link, function(obj) {
@@ -10,7 +11,14 @@ $(document).ready(function() {
 });
 
 SetHTMLPreInstall = (value) =>{
+    //On Game Files
     document.getElementById('latest-before-predl').innerHTML = value.diffs[0].version
     document.getElementById('latest-after-predl').innerHTML = value.latest.version
     document.getElementById('link-predl').setAttribute("href", value.diffs[0].path)
+
+    //On English Voice Pack
+    document.getElementById('link-predl-en').setAttribute("href", value.diffs[0].voice_packs[1].path)
+
+    //On Japanese Voice Pack
+    document.getElementById('link-predl-jp').setAttribute("href", value.diffs[0].voice_packs[2].path)
 }
